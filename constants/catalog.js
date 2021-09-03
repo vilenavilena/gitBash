0984075815b63ce2b49fe9b4f456f12905aee66c
Vilena <vilena.niyaz@gmail.com>
2021-09-03 11:47:55 +0500

    компонент католог товаров

---
+++ constants/catalog.js
@@ -0,0 +1,56 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: 'FENDER SQUIER MM STRATOCASTER HARD TAIL BLACK',
+        img: 'img/catalog/1.jpg',
+        price: 13000,
+    },
+    {
+        id: 'el2',
+        name: 'FENDER SQUIER MM STRATOCASTER HARD TAIL RED',
+        img: 'img/catalog/2.jpg',
+        price: 14000,
+    },
+    {
+        id: 'el3',
+        name: 'EPIPHONE Les Paul Special VE Ebony Vintage',
+        img: 'img/catalog/3.jpg',
+        price: 23400,
+    },
+    {
+        id: 'el4',
+        name: 'IBANEZ GRX20-BKN BLACK',
+        img: 'img/catalog/4.jpg',
+        price: 19200,
+    },
+    {
+        id: 'el5',
+        name: 'FENDER SQUIER BULLET STRAT HT AWT',
+        img: 'img/catalog/5.jpg',
+        price: 19500,
+    },
+    {
+        id: 'el6',
+        name: 'EPIPHONE SG-Special VE Cherry',
+        img: 'img/catalog/6.jpg',
+        price: 23400,
+    },
+    {
+        id: 'el7',
+        name: 'EPIPHONE Les Paul Special VE Vintage Sunburst',
+        img: 'img/catalog/7.jpg',
+        price: 23400,
+    },
+    {
+        id: 'el8',
+        name: 'IBANEZ RGA42FM-BLF',
+        img: 'img/catalog/8.jpg',
+        price: 43000,
+    },
+    {
+        id: 'el9',
+        name: 'EPIPHONE SLASH AFD LES PAUL SPECIAL-II OUTFIT',
+        img: 'img/catalog/9.jpg',
+        price: 29000,
+    },
+];
